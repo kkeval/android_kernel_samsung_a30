af6ac6014fedb6572f74e828c34c059d1601fe82
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-04-14 13:26:59 +0530

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 10 18:51:43 IST 2020"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "machine"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
